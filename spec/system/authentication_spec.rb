d20c61fa74135d2c6dc62071725f4e660e5bc18d
sekito <sekito1107gg@gmail.com>
2025-12-04 19:18:53 +0900

    認証テストから不要なenv_config設定を削除
    System SpecではOmniAuthミドルウェアが正常に動作し、mock_authを自動的に参照するため、Rails.application.env_configへの手動代入は不要でした。
    テストの意図を明確にし、不要なコードを削除しました。

--- spec/system/authentication_spec.rb
+++ spec/system/authentication_spec.rb
@@ -3,10 +3,9 @@
 require "rails_helper"
 
 RSpec.describe "Authentication", type: :system do
-  before do
-    OmniAuth.config.mock_auth[:github] = nil
-    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
-  end
+  # before do
+  #   OmniAuth.config.mock_auth[:github] = nil
+  # end
 
   context "未ログインの場合" do
     it "トップページからGitHub認証でログインできること" do
