d011baf157d4de08808c27e4b16d450a2c3035a0
sekito <sekito1107gg@gmail.com>
2025-12-06 04:04:44 +0900

    一部表示用処理をヘルパーに切り出しました

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -24,4 +24,49 @@ module ExamsHelper
       }
     end
   end
+
+  def reveal_animation_classes(needs_animation)
+    {
+      wrapper: needs_animation ? "opacity-0 translate-y-4" : "",
+      fade:    needs_animation ? "opacity-0" : ""
+    }
+  end
+
+  def exam_chart_attributes(exam, needs_animation)
+    percentage = exam.score_percentage
+    passed = exam.passed?
+    circumference = 283 # 2 * PI * 45 ≈ 282.7
+    offset = circumference - (percentage / 100.0 * circumference)
+
+    if needs_animation
+      {
+        initial_color: "text-slate-200",
+        style: "stroke-dasharray: 0, #{circumference};",
+        initial_score: "0"
+      }
+    else
+      color_class = passed ? "text-emerald-500" : "text-red-500"
+      {
+        initial_color: color_class,
+        style: "stroke-dasharray: #{circumference}, #{circumference}; stroke-dashoffset: #{offset};",
+        initial_score: percentage.to_i
+      }
+    end
+  end
+
+  def exam_result_message(passed)
+    if passed
+      {
+        title: "PASSED!",
+        title_class: "text-emerald-500",
+        description: "おめでとうございます！<br>合格ラインを突破しました。"
+      }
+    else
+      {
+        title: "FAILED...",
+        title_class: "text-red-500",
+        description: "残念ながら不合格です。<br>復習して再挑戦しましょう。"
+      }
+    end
+  end
 end
