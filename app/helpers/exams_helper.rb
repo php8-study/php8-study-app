8e652215a28a66825300a54d30d4ce6289aea9ef
sekito <sekito1107gg@gmail.com>
2025-12-06 00:04:15 +0900

    表示ロジックをヘルパーに切り出した

---
+++ app/helpers/exams_helper.rb
@@ -0,0 +1,25 @@
+module ExamsHelper
+  def exam_status_theme(exam)
+    if exam.passed?
+      {
+        wrapper_class: "border-slate-100 hover:border-emerald-200 hover:bg-emerald-50/30",
+        accent_border: "border-l-emerald-500",
+        text_main: "text-emerald-700",
+        calendar_bg: "bg-emerald-100/50 text-emerald-800",
+        calendar_label: "text-emerald-600/70",
+        stamp_text: "PASSED",
+        stamp_style: "text-emerald-600 border-emerald-600 bg-emerald-100/50 -rotate-12",
+      }
+    else
+      {
+        wrapper_class: "border-slate-100 hover:border-red-200 hover:bg-red-50/30",
+        accent_border: "border-l-red-500",
+        text_main: "text-red-700",
+        calendar_bg: "bg-red-100/50 text-red-800",
+        calendar_label: "text-red-600/70",
+        stamp_text: "FAILED",
+        stamp_style: "text-red-600 border-red-600 bg-red-100/50 rotate-6",
+      }
+    end
+  end
+end
