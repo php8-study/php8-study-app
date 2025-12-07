6422c7a093a88a9ef0aeed955815ba1a3dac0ce1
sekito <sekito1107gg@gmail.com>
2025-12-08 07:01:48 +0900

    lintによる修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,7 +4,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :selenium_chrome_headless, screen_size: [1400, 1400] do |options|
       options.add_argument("disable-dev-shm-usage")
-      options.add_argument('disable-gpu')
+      options.add_argument("disable-gpu")
     end
   end
 end
