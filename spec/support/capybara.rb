20514dce9669d516ea78f47e6c7192cf354918ff
sekito <sekito1107gg@gmail.com>
2025-12-08 06:36:57 +0900

    chromeがメモリ不足でクラッシュするので、共有メモリの代わりにディスクを使うように

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,6 +2,8 @@
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
+    driven_by :selenium_chrome_headless, screen_size: [1400, 1400] do |options|
+      options.add_argument('disable-dev-shm-usage')
+    end
   end
 end
