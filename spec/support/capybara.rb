42cf7d8224f4133d3289b1d1f227db2ec81cd07a
sekito <sekito1107gg@gmail.com>
2025-12-08 17:52:44 +0900

    ブラウザの設定を変更した

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,9 +2,16 @@
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless, screen_size: [1400, 1400] do |options|
-      options.add_argument("disable-dev-shm-usage")
-      options.add_argument("disable-gpu")
+    # :selenium_chrome_headless ではなく :selenium を指定
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |options|
+      # メモリ不足対策（必須）
+      options.add_argument('disable-dev-shm-usage')
+      # サンドボックス無効化（必須）
+      options.add_argument('no-sandbox')
+      # GPU無効化
+      options.add_argument('disable-gpu')
+      # 新しいヘッドレスモード（安定性が高い）
+      options.add_argument('headless=new')
     end
   end
 end
