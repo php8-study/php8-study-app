e158248500d6f1f0ba5842d54138244ce5cd3811
sekito <sekito1107gg@gmail.com>
2025-12-08 18:11:57 +0900

    Chrome オプション引数を修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,16 +2,11 @@
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    # :selenium_chrome_headless ではなく :selenium を指定
     driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |options|
-      # メモリ不足対策（必須）
-      options.add_argument("disable-dev-shm-usage")
-      # サンドボックス無効化（必須）
-      options.add_argument("no-sandbox")
-      # GPU無効化
-      options.add_argument("disable-gpu")
-      # 新しいヘッドレスモード（安定性が高い）
-      options.add_argument("headless=new")
+      options.add_argument("--disable-dev-shm-usage")
+      options.add_argument("--no-sandbox")
+      options.add_argument("--disable-gpu")
+      options.add_argument("--headless=new")
     end
   end
 end
