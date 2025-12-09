b481b2cd9329b62c7195df1e8b4f33327965ba54
sekito <sekito1107gg@gmail.com>
2025-12-10 02:24:47 +0900

    管理画面TOPのシステムテストをリファクタリング

--- spec/system/admin_spec.rb
+++ spec/system/admin_spec.rb
@@ -13,24 +13,10 @@ RSpec.describe "管理画面", type: :system do
   describe "一覧" do
     it "管理メニューが表示され、各リンクが配置されていること" do
       expect(page).to have_content "管理画面"
-      expect(page).to have_link "問題一覧"
-      expect(page).to have_link "カテゴリー一覧"
-      expect(page).to have_link "ユーザー画面に戻る"
-    end
 
-    it "各リンクが正常に動作すること", :aggregate_failures do
-      click_link "問題一覧"
-      expect(page).to have_current_path(admin_questions_path)
-
-      visit admin_root_path
-
-      click_link "カテゴリー一覧"
-      expect(page).to have_current_path(admin_categories_path)
-
-      visit admin_root_path
-
-      click_link "ユーザー画面に戻る"
-      expect(page).to have_current_path(root_path)
+      expect(page).to have_link "問題一覧", href: admin_questions_path
+      expect(page).to have_link "カテゴリー一覧", href: admin_categories_path
+      expect(page).to have_link "ユーザー画面に戻る", href: root_path
     end
   end
 end
