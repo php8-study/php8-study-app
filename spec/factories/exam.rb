a225ca73216ee642a44e3bdd14ed0b19f29c4d21
sekito <sekito1107gg@gmail.com>
2025-12-10 18:02:10 +0900

    examモデルをテスト内で生成する時、合格と不合格をtraitでえらべるように実装

--- spec/factories/exam.rb
+++ spec/factories/exam.rb
@@ -8,5 +8,23 @@ FactoryBot.define do
     trait :completed do
       completed_at { Time.current }
     end
+
+    trait :passed do
+      completed
+      after(:create) do |exam|
+        question = create(:question, :with_choices)
+        eq = create(:exam_question, exam: exam, question: question)
+        correct_choice = question.question_choices.find_by(correct: true)
+        create(:exam_answer, exam_question: eq, question_choice: correct_choice)
+      end
+    end
+
+    trait :failed do
+      completed
+      after(:create) do |exam|
+        question = create(:question, :with_choices)
+        create(:exam_question, exam: exam, question: question)
+      end
+    end
   end
 end
