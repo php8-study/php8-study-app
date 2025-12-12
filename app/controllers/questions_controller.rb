fad7136e33bb0fdd4bf34adee700412f18ba7ec0
sekito <sekito1107gg@gmail.com>
2025-12-13 02:52:16 +0900

    論理削除された問題にはアクセスもできないように変更

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,6 +24,6 @@ class QuestionsController < ApplicationController
 
   private
     def set_question
-      @question = Question.find(params[:id])
+      @question = Question.active.find(params[:id])
     end
 end
