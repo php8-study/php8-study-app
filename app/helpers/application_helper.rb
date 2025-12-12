0f1da133492c3d29aa0b5760cfdcf4c5cd9d4042
sekito <sekito1107gg@gmail.com>
2025-12-12 22:18:50 +0900

    セキュリティ対策にrequest.original_urlの使用を取りやめ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
       separator: "|",
       description: "【完全無料】PHP8技術者認定初級試験の合格を目指すための学習サイトです。数百問に及ぶ演習問題、本番形式の模擬試験、ランダム出題機能で効率よく学習可能。公式教材への参照付きで、基礎固めから試験直前の総仕上げまで強力にサポートします。",
       keywords: "PHP, PHP8, 技術者認定初級試験, 初級, 問題集, 模擬試験, 過去問, 無料, 資格, 独学, 勉強法, 対策, アプリ",
-      canonical: request.original_url,
+      canonical: "#{request.base_url}#{request.path}",
       noindex: ! Rails.env.production?,
       icon: [
         { href: image_url("favicon.ico") },
@@ -20,7 +20,7 @@ module ApplicationHelper
         title: :title,
         description: :description,
         type: "website",
-        url: request.original_url,
+        url: "#{request.base_url}#{request.path}",
         image: image_url("ogp.png"),
         locale: "ja_JP",
       },
