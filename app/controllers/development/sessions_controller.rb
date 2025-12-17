016a06afa76ac2a3d9c84bc9c057adf61118d37c
sekito <sekito1107gg@gmail.com>
2025-12-17 22:22:33 +0900

    lintによる修正

--- app/controllers/development/sessions_controller.rb
+++ app/controllers/development/sessions_controller.rb
@@ -6,22 +6,21 @@ module Development
     skip_before_action :require_login, raise: false
 
     private
-
-    def ensure_development_environment
-      raise ActionController::RoutingError, 'Not Found' unless Rails.env.development?
-    end
-
-
-    def sign_in_as
-      user = User.find(params[:user_id])
-      unless user
-        redirect_to root_path, alert: "指定されたユーザーが見つかりません"
-        return
+      def ensure_development_environment
+        raise ActionController::RoutingError, "Not Found" unless Rails.env.development?
       end
-      
-      session[:user_id] = user.id
 
-      redirect_to root_path, notice: "開発用: #{user.admin? ? '管理者' : '一般ユーザー'} (ID: #{user.id}) としてログインしました"
-    end
+
+      def sign_in_as
+        user = User.find(params[:user_id])
+        unless user
+          redirect_to root_path, alert: "指定されたユーザーが見つかりません"
+          return
+        end
+
+        session[:user_id] = user.id
+
+        redirect_to root_path, notice: "開発用: #{user.admin? ? '管理者' : '一般ユーザー'} (ID: #{user.id}) としてログインしました"
+      end
   end
 end
