c95e1709ee1a10887ca37471d3a80847cbd9781e
sekito <sekito1107gg@gmail.com>
2025-12-19 01:16:55 +0900

    setアクション内ではincludeを行わないように

--- app/controllers/exams/reviews_controller.rb
+++ app/controllers/exams/reviews_controller.rb
@@ -6,14 +6,14 @@ module Exams
     before_action :ensure_in_progress
 
     def show
-      @exam_questions = @exam.exam_questions.includes(:exam_answers).order(position: :asc)
+      @exam_questions = @exam.exam_questions
+                             .includes(:exam_answers, question: :question_choices)
+                             .order(position: :asc)
     end
 
     private
       def set_exam
-        @exam = current_user.exams
-                           .includes(exam_questions: [{ question: :question_choices }, :exam_answers])
-                           .find(params[:exam_id])
+        @exam = current_user.exams.find(params[:exam_id])
       end
 
       def ensure_in_progress
