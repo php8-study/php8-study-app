3bc4bfcb9f08095ad40a2ae2c911633f92f0274b
sekito <sekito1107gg@gmail.com>
2025-12-19 03:15:14 +0900

    パラメータ処理をprivateメソッドに隠蔽

--- app/controllers/questions/solutions_controller.rb
+++ app/controllers/questions/solutions_controller.rb
@@ -5,13 +5,16 @@ class Questions::SolutionsController < ApplicationController
   skip_before_action :require_login, only: [:show]
 
   def show
-    user_answer_ids = params[:user_answer_ids]
-    @is_correct = @question.answer_correct?(user_answer_ids)
-    @user_answer_ids = Array(user_answer_ids).map(&:to_i)
+    @user_answer_ids = parsed_answer_ids
+    @is_correct = @question.answer_correct?(@user_answer_ids)
   end
 
   private
     def set_question
       @question = Question.active.find(params[:question_id])
+    end
+
+    def parsed_answer_ids
+      Array(params[:user_answer_ids]).map(&:to_i)
     end
 end
