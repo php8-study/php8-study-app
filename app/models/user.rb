5da9d9d16662ef9e399e17c04bfa8ca13e3bfbb3
sekito <sekito1107gg@gmail.com>
2025-12-18 18:26:46 +0900

    userから不要なメソッドを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,4 @@ class User < ApplicationRecord
   def active_exam
     exams.find_by(completed_at: nil)
   end
-
-  def discard_active_exam
-    if target_exam = active_exam
-      target_exam.destroy!
-    end
-  rescue ActiveRecord::ActiveRecordError => e
-    Rails.logger.error("Failed to discard active exam for User #{id}: #{e.message}")
-  end
 end
