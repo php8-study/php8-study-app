d35927da7e6821a5d05f0de5223af88ec98c3f64
sekito <sekito1107gg@gmail.com>
2025-12-19 19:48:54 +0900

    テストを更新し、テストの期待値を一部変更

--- spec/components/exams/sticky_nav/component_spec.rb
+++ spec/components/exams/sticky_nav/component_spec.rb
@@ -3,29 +3,28 @@
 require "rails_helper"
 
 RSpec.describe Exams::StickyNav::Component, type: :component do
-  context "コンテンツの描画" do
-    it "ブロックで渡した内容が表示されること" do
-      render_inline(described_class.new) do
-        "<button>次のアクション</button>".html_safe
-      end
 
-      expect(page).to have_button("次のアクション")
+  context "コンテンツの描画" do
+    before do
+      render_inline(described_class.new)
+    end
+
+    it "ナビゲーションボタンが表示されること" do
+      expect(page).to have_link("トップへ戻る")
+      expect(page).to have_link("履歴一覧へ戻る")
+    end
+
+    it "スコアが表示されること" do
+      expect(page).to have_content("#{exam.score_percentage}%")
     end
   end
 
   context "アニメーション設定" do
-    it "animation: true の場合、初期状態で非表示（透明・位置ズレ）クラスが付与されること" do
-      render_inline(described_class.new(animation: true)) { "ボタン" }
+    it "animation: true の場合、非表示クラスが付与されること" do
+      render_inline(described_class.new(exam: exam, animation: true))
 
-      expect(page).to have_selector(".opacity-0")
-      expect(page).to have_selector(".translate-y-4")
-    end
-
-    it "animation: false の場合、非表示クラスが付与されないこと" do
-      render_inline(described_class.new(animation: false)) { "ボタン" }
-
-      expect(page).to have_no_selector(".opacity-0")
-      expect(page).to have_no_selector(".translate-y-4")
+      wrapper = page.find("[data-controller='sticky-nav']")
+      expect(wrapper[:class]).to include("opacity-0")
     end
   end
 end
