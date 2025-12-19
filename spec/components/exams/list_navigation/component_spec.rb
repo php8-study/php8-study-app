6c15d22d9ff7c4dcbff76ef2a0bb25f435eb5113
sekito <sekito1107gg@gmail.com>
2025-12-20 04:36:39 +0900

    テストの期待値をリンクがあるかどうかだけに変更

--- spec/components/exams/list_navigation/component_spec.rb
+++ spec/components/exams/list_navigation/component_spec.rb
@@ -8,14 +8,10 @@ RSpec.describe Exams::ListNavigation::Component, type: :component do
   end
 
   it "ダッシュボードへのボタンが表示され、正しいリンクが設定されていること" do
-    expect(page).to have_link("ダッシュボードへ", href: root_path)
-
-    expect(page).to have_css("a.bg-white")
+    expect(page).to have_css("a[href='#{root_path}']")
   end
 
   it "新規試験作成へのボタンが表示され、正しいリンクが設定されていること" do
-    expect(page).to have_link("新しい模擬試験を受ける", href: check_exams_path)
-
-    expect(page).to have_css("a.bg-indigo-600")
+    expect(page).to have_css("a[href='#{check_exams_path}']")
   end
 end
