f9b76bef933f3d18f693cccd31335d2acb264315
sekito <sekito1107gg@gmail.com>
2025-12-20 04:12:41 +0900

    簡単なテストを作成

---
+++ spec/components/exams/list_navigation/component_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Exams::ListNavigation::Component, type: :component do
+  before do
+    render_inline(described_class.new)
+  end
+
+  it "ダッシュボードへのボタンが表示され、正しいリンクが設定されていること" do
+    expect(page).to have_link("ダッシュボードへ", href: root_path)
+    
+    expect(page).to have_css("a.bg-white") 
+  end
+
+  it "新規試験作成へのボタンが表示され、正しいリンクが設定されていること" do
+    expect(page).to have_link("新しい模擬試験を受ける", href: check_exams_path)
+
+    expect(page).to have_css("a.bg-indigo-600")
+  end
+end
