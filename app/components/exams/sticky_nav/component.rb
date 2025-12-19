efc3a2b5eba7d3eb1ce4332918863712514b13a0
sekito <sekito1107gg@gmail.com>
2025-12-19 20:02:59 +0900

    アニメーション用の表示を一本化

--- app/components/exams/sticky_nav/component.rb
+++ app/components/exams/sticky_nav/component.rb
@@ -12,7 +12,7 @@ module Exams
           classes = %w[fixed bottom-0 left-0 w-full z-50 transition-transform duration-500]
 
           if @animation
-            classes << "translate-y-full opacity-0 translate-y-4"
+            classes << "translate-y-full opacity-0"
           end
 
           classes.join(" ")
