0fe9361e8fca6a5bd916afb5f42ee599c12ee74f
sekito <sekito1107gg@gmail.com>
2025-12-20 08:07:10 +0900

    解説画面のリファレンスパーツをコンポーネントで再実装

---
+++ app/components/questions/reference_info/component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Questions
+  module ReferenceInfo
+    class Component < ViewComponent::Base
+      def initialize(question:)
+        @question = question
+      end
+
+      private
+        def official_page
+          @question.official_page
+        end
+
+        def present?
+          official_page.present?
+        end
+    end
+  end
+end
