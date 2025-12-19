7b5afadfeab61c4be05340fa54c972a0b2d9e6ce
sekito <sekito1107gg@gmail.com>
2025-12-19 19:46:32 +0900

    sticky_navにパーシャルを取り込み1本化

--- app/components/exams/sticky_nav/component.rb
+++ app/components/exams/sticky_nav/component.rb
@@ -7,29 +7,17 @@ module Exams
         @animation = animation
       end
 
-      def call
-        tag.div(
-          data: {
-            controller: "sticky-nav",
-            result_reveal_target: "actionButtons",
-            sticky_nav_footer_selector_value: "#global-footer"
-          },
-          class: footer_classes
-        ) do
-          content
-        end
-      end
-
       private
-        def footer_classes
-          classes = %w[fixed bottom-0 left-0 w-full z-50 transition-transform duration-500]
 
-          if @animation
-            classes << "translate-y-full opacity-0 translate-y-4"
-          end
+      def nav_classes
+        classes = %w[fixed bottom-0 left-0 w-full z-50 transition-transform duration-500]
 
-          classes.join(" ")
+        if @animation
+          classes << "translate-y-full opacity-0 translate-y-4"
         end
+
+        classes.join(" ")
+      end
     end
   end
 end
