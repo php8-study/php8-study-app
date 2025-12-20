d119d2d7e093c2046874c2bb5fadb7ad81d87ef2
sekito <sekito1107gg@gmail.com>
2025-12-20 09:58:37 +0900

    ヘッダー部分がロジック過多だったのでパーシャルに切り出し

---
+++ app/components/exam_questions/header/component.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module ExamQuestions
+  module Header
+    class Component < ViewComponent::Base
+      def initialize(exam:, exam_question:)
+        @exam = exam
+        @exam_question = exam_question
+      end
+
+      private
+
+        def position
+          @exam_question.position
+        end
+
+        def total
+          @exam.exam_questions.size
+        end
+    end
+  end
+end
