48f3af1103d95d2abc507c6d017525c60972d3a8
sekito <sekito1107gg@gmail.com>
2025-12-20 22:13:43 +0900

    ファイル構造を変更、それに伴い呼び出し方も変更

---
+++ app/components/admin/categories/list/row/component.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Admin
+  module Categories
+    module List
+      module Row
+        class Component < ViewComponent::Base
+          with_collection_parameter :category
+
+          def initialize(category:, grid_cols:)
+            @category = category
+            @grid_cols = grid_cols
+          end
+
+          private
+            def progress_bar_style
+              "width: #{@category.weight}%"
+            end
+        end
+      end
+    end
+  end
+end
