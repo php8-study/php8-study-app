d59fe2e6a15511d4aacf35fdd2517ac982708bfa
sekito <sekito1107gg@gmail.com>
2025-12-20 09:52:38 +0900

    カード毎コンポーネントに切り出し

---
+++ app/components/exams/check/content_component.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Exams
+  module Check
+    class ContentComponent < ViewComponent::Base
+      def initialize(active_exam:)
+        @active_exam = active_exam
+      end
+
+      private
+        def formatted_date
+          @active_exam.created_at.strftime("%Y年%m月%d日 %H:%M")
+        end
+    end
+  end
+end
