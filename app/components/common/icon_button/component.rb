a7b9928946d2e3dcc2a30eac5f884eb957d4975e
sekito <sekito1107gg@gmail.com>
2025-12-23 04:47:18 +0900

    テストの期待値を修正

--- app/components/common/icon_button/component.rb
+++ app/components/common/icon_button/component.rb
@@ -4,34 +4,41 @@ module Common
   module IconButton
     class Component < ViewComponent::Base
       VARIANTS = {
-        primary: "text-indigo-600 hover:bg-indigo-50",
-        secondary:   "text-slate-400 hover:text-slate-600 hover:bg-slate-100",
+        primary:   "text-indigo-600 hover:bg-indigo-50",
+        secondary: "text-slate-400 hover:text-slate-600 hover:bg-slate-100",
         danger:    "text-red-600 hover:bg-red-50"
       }.freeze
 
-      def initialize(href:, icon:, variant: :primary, title: nil, method: :get, confirm: nil, data: {})
+      def initialize(icon:, href: nil, variant: :primary, title: nil, type: :button, method: :get, confirm: nil, data: {})
         @href = href
         @icon = icon
         @variant = VARIANTS[variant] || VARIANTS[:primary]
         @title = title
+        @type = type
         @method = method
         @confirm = confirm
         @data = data
       end
 
       def call
-        helpers.link_to(@href, class: merged_classes, title: @title, aria: { label: @title }, data: data_attributes) do
-          helpers.inline_svg_tag(icon_path, class: "w-5 h-5", aria_hidden: true)
+        if @href
+          helpers.link_to(@href, class: merged_classes, title: @title, aria: { label: @title }, data: link_data_attributes) do
+            render_icon
+          end
+        else
+          helpers.button_tag(type: @type, class: merged_classes, title: @title, aria: { label: @title }, data: @data) do
+            render_icon
+          end
         end
       end
 
       private
 
-        def icon_path
-          "icons/#{@icon}.svg"
+        def render_icon
+          helpers.inline_svg_tag("icons/#{@icon}.svg", class: "w-5 h-5", aria_hidden: true)
         end
 
-        def data_attributes
+        def link_data_attributes
           attributes = @data.dup
           attributes[:turbo_method] = @method if @method != :get
           attributes[:turbo_confirm] = @confirm if @confirm.present?
--- spec/components/admin/categories/list/row/form/component_spec.rb
+++ spec/components/admin/categories/list/row/form/component_spec.rb
@@ -3,7 +3,6 @@
 require "rails_helper"
 
 RSpec.describe Admin::Categories::List::Row::Form::Component, type: :component do
-  # dom_id を使うために必要
   include ActionView::RecordIdentifier
 
   let(:category) { create(:category, name: "編集対象", chapter_number: 9, weight: 80.0) }
@@ -19,14 +18,16 @@ RSpec.describe Admin::Categories::List::Row::Form::Component, type: :component d
   end
 
   it "既存の値が入力フィールドにセットされていること" do
-    expect(page).to have_selector("input[aria-label='タイトル'][value='編集対象']")
-    expect(page).to have_selector("input[aria-label='章番号'][value='9']")
-    expect(page).to have_selector("input[aria-label='出題割合(%)'][value='80.0']")
+    expect(page).to have_selector("input[name='category[name]'][value='編集対象']")
+    expect(page).to have_selector("input[name='category[chapter_number]'][value='9']")
+    expect(page).to have_selector("input[name='category[weight]'][value='80.0']")
   end
 
   it "操作ボタンが表示されること" do
-    expect(page).to have_selector("button[aria-label='変更を保存']")
-    expect(page).to have_selector("a[aria-label='編集をキャンセル'][href='#{admin_category_path(category)}']")
+    expect(page).to have_selector("button[type='submit']")
+
+    expect(page).to have_link(href: admin_category_path(category))
+    
     expect(page).to have_selector("a[data-turbo-frame='#{dom_id(category)}']")
   end
 end
