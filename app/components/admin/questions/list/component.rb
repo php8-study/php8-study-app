8374da773be1e3cb474eed5a5d34a80a309b0507
sekito <sekito1107gg@gmail.com>
2025-12-23 02:58:38 +0900

    th作成をメソッド化

--- app/components/admin/questions/list/component.rb
+++ app/components/admin/questions/list/component.rb
@@ -7,6 +7,15 @@ module Admin
         def initialize(questions:)
           @questions = questions
         end
+
+        private
+
+          def header_th(text, align: :left, classes: "px-3")
+            base_classes = "py-3.5 text-xs font-bold uppercase tracking-wider text-slate-500"
+            alignment_class = "text-#{align}"
+
+            content_tag :th, text, scope: "col", class: "#{base_classes} #{alignment_class} #{classes}"
+          end
       end
     end
   end
