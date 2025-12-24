21e45794bd92a2d09a6e0dd1d9656d6fda218ea2
sekito <sekito1107gg@gmail.com>
2025-12-25 04:20:37 +0900

    問題文中のコードをコードブロックとして出力できるように変更

--- app/components/common/terminal_display/component.rb
+++ app/components/common/terminal_display/component.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'rouge/plugins/redcarpet'
 
 module Common
   module TerminalDisplay
@@ -6,6 +7,29 @@ module Common
       def initialize(body:, label: "Question.php")
         @body = body
         @label = label
+      end
+
+      private
+
+      def formatted_body
+        renderer = HTMLWithRouge.new(
+          filter_html: true,
+          hard_wrap: true,
+          link_attributes: { rel: 'nofollow', target: "_blank" }
+        )
+
+        markdown = Redcarpet::Markdown.new(renderer,
+          fenced_code_blocks: true,
+          autolink: true,
+          tables: true,
+          strikethrough: true
+        )
+
+        markdown.render(@body).html_safe
+      end
+
+      class HTMLWithRouge < Redcarpet::Render::HTML
+        include Rouge::Plugins::Redcarpet
       end
     end
   end
