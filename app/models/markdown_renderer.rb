faeb949254bfaa442385f8751a32c8a756dfcf97
sekito <sekito1107gg@gmail.com>
2025-12-25 04:31:00 +0900

    レンダリング機能をmoduleに切り出した

---
+++ app/models/markdown_renderer.rb
@@ -0,0 +1,26 @@
+module MarkdownRenderer
+  OPTIONS = {
+    filter_html: true,
+    hard_wrap: true,
+    link_attributes: { rel: "nofollow", target: "_blank" }
+  }.freeze
+
+  EXTENSIONS = {
+    fenced_code_blocks: true,
+    autolink: true,
+    tables: true,
+    strikethrough: true
+  }.freeze
+
+  def self.render(text)
+    return "" if text.blank?
+
+    renderer = HTMLWithRouge.new(OPTIONS)
+    markdown = Redcarpet::Markdown.new(renderer, EXTENSIONS)
+    markdown.render(text).html_safe
+  end
+
+  class HTMLWithRouge < Redcarpet::Render::HTML
+    include Rouge::Plugins::Redcarpet
+  end
+end
