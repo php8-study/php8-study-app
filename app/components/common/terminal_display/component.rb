3458fcc0af22ccc4130f4d4afb5b41984a37979f
sekito <sekito1107gg@gmail.com>
2025-12-25 04:50:56 +0900

    lintによる修正

--- app/components/common/terminal_display/component.rb
+++ app/components/common/terminal_display/component.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Common
   module TerminalDisplay
     class Component < ViewComponent::Base
--- app/models/markdown_renderer.rb
+++ app/models/markdown_renderer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module MarkdownRenderer
   OPTIONS = {
     filter_html: true,
