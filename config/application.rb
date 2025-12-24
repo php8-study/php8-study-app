53dfbc9ca6edc16706bd415371be842a804d2673
sekito <sekito1107gg@gmail.com>
2025-12-25 05:09:06 +0900

    CIでのエラーを避けるため、事前にプラグインをロードしておくように変更

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require "rouge/plugins/redcarpet"
+
 module Php8Study
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
