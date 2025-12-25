b0ac6d65835af9e9f18a202d2a591f4bede8c499
sekito <sekito1107gg@gmail.com>
2025-12-26 07:35:18 +0900

    コミット忘れ

--- app/components/common/explanation/component.rb
+++ app/components/common/explanation/component.rb
@@ -3,12 +3,22 @@
 module Common
   module Explanation
     class Component < ViewComponent::Base
+      INLINE_CODE_STYLE = "font-mono text-sm text-red-400 bg-slate-100 rounded px-1.5 py-0.5".freeze
+
       def initialize(question:)
         @question = question
       end
 
       def render?
         @question.explanation.present?
+      end
+      private
+
+      def formatted_explanation
+        MarkdownRenderer.render(
+          @question.explanation, 
+          inline_code_style: INLINE_CODE_STYLE
+        )
       end
     end
   end
