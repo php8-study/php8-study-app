5028cd0446defd598625467ac1a2ce129a987689
sekito <sekito1107gg@gmail.com>
2025-12-27 00:58:17 +0900

    インスタンス化しないので、moduleに差し戻した

--- app/models/markdown_content.rb
+++ app/models/markdown_content.rb
@@ -21,14 +21,29 @@ class MarkdownContent
   def to_html
     return "" if @raw_text.blank?
 
-    renderer = HTMLWithRouge.new(OPTIONS)
+    renderer = HTMLWithRouge.new(OPTIONS.merge(inline_code_style: inline_code_style))
     markdown = Redcarpet::Markdown.new(renderer, EXTENSIONS)
 
     markdown.render(@raw_text).html_safe
   end
 
+  module_function :render
+
   class HTMLWithRouge < Redcarpet::Render::HTML
     include Rouge::Plugins::Redcarpet
+
+    def initialize(extensions = {})
+      @inline_code_style = extensions.delete(:inline_code_style)
+      super(extensions)
+    end
+
+    def codespan(code)
+      if @inline_code_style
+        "<code class=\"#{@inline_code_style}\">#{code}</code>"
+      else
+        "<code>#{code}</code>"
+      end
+    end
   end
 
   private_constant :HTMLWithRouge
