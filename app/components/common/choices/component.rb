5acff506825c6a0f25893df2767dbd7f1a4fbee8
sekito <sekito1107gg@gmail.com>
2025-12-31 00:22:33 +0900

    選択肢でもマークダウン記法を使えるように調整

--- app/components/common/choices/component.rb
+++ app/components/common/choices/component.rb
@@ -3,11 +3,26 @@
 module Common
   module Choices
     class Component < ViewComponent::Base
+      WRAPPER_STYLE = %w[
+        [&_code]:font-mono
+        [&_code]:text-sm
+        [&_code]:text-slate-700
+        [&_code]:bg-slate-100
+        [&_code]:rounded
+        [&_code]:leading-relaxed
+        [&_code]:px-1.5
+        [&_code]:py-0.5
+      ].join(" ").freeze
+
       def initialize(question:, input_name: nil, selected_choice_ids: [], disabled: false)
         @question = question
         @input_name = input_name
         @selected_choice_ids = Array(selected_choice_ids).map(&:to_i)
         @disabled = disabled
+      end
+
+      def formatted_content(content)
+        MarkdownContent.new(content).to_html
       end
 
       private
@@ -21,8 +36,11 @@ module Common
 
         def text_classes
           classes = %w[
-          block text-base text-slate-700 group-hover:text-indigo-900 transition-colors
+            block text-base text-slate-700 group-hover:text-indigo-900 transition-colors
           ]
+
+          classes << WRAPPER_STYLE
+
           classes << "peer-checked:font-bold" unless @disabled
           classes.join(" ")
         end
