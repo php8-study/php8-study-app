e8e2c56acf3ae5b86ecec3d601a294e361b049e2
sekito <sekito1107gg@gmail.com>
2025-12-31 09:28:59 +0900

    キャッシュを使うよう変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,10 @@ class HomeController < ApplicationController
   skip_before_action :require_login, only: [:index]
 
   def index
-    if current_user
-    # デフォルトの index ビューをレンダリング
-    else
-      @questions_count = Question.active.count
+    unless current_user
+      @questions_count = Rails.cache.fetch("active_questions_count", expires_in: 1.hour) do
+        Question.active.count
+      end
       render :landing
     end
   end
