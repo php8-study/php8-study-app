d27ba30456a7f1ef7527b83b356937735800e23d
sekito <sekito1107gg@gmail.com>
2026-01-01 16:58:25 +0900

    セキュリティを厳密に変更

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
     policy.font_src    :self, :https, :data, "https://fonts.gstatic.com"
     policy.img_src     :self, :https, :data
     policy.object_src  :none
-    policy.script_src  :self, :https
+    policy.script_src  :self
     policy.style_src   :self, :https, "https://fonts.googleapis.com"
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Automatically add `nonce` to `javascript_tag`, `javascript_include_tag`, and `stylesheet_link_tag`
   # if the corresponding directives are specified in `content_security_policy_nonce_directives`.
-  # config.content_security_policy_nonce_auto = true
+  config.content_security_policy_nonce_auto = true
 
   # Report violations without enforcing the policy.
   # config.content_security_policy_report_only = true
