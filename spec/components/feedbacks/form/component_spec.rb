500342736ad1fee13433755b6643eb39dfc96189
sekito <sekito1107gg@gmail.com>
2026-01-03 09:38:22 +0900

    lintの修正

--- spec/components/feedbacks/form/component_spec.rb
+++ spec/components/feedbacks/form/component_spec.rb
@@ -6,18 +6,21 @@ RSpec.describe Feedbacks::Form::Component, type: :component do
   let(:title) { "問題を報告する" }
   let(:question_id) { 123 }
 
-  it "リンクテキストが正しく表示されること" do
+  it "ボタンテキストが正しく表示されること" do
     render_inline(described_class.new(title: title))
-    expect(page).to have_link(title)
+    expect(page).to have_button(title)
   end
 
-  it "question_idが渡された時、リンクのURLパラメータに正しく含まれること" do
+  it "question_idが渡された時、フォームのhiddenフィールドに正しく含まれること" do
     render_inline(described_class.new(title: title, question_id: question_id))
-    expect(page).to have_link(title, href: /question_id=#{question_id}/)
+
+    expect(page).to have_field("question_id", type: "hidden", with: question_id.to_s)
+
+    expect(page).to have_selector("form[action*='#{new_feedback_path}']")
   end
 
   it "Turbo Stream形式のリクエストを送る設定になっていること" do
     render_inline(described_class.new(title: title))
-    expect(page).to have_css("a[data-turbo-stream='true']")
+    expect(page).to have_selector("form[data-turbo-stream='true']")
   end
 end
