dcbbd3819566a9c7cb84cfeaaf730f321e336ab8
sekito <sekito1107gg@gmail.com>
2026-01-03 09:52:38 +0900

    ストロングパラメータを使用

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,13 +2,14 @@
 
 class FeedbacksController < ApplicationController
   skip_before_action :require_login
+
   def new
     @question_id = params[:question_id]
   end
 
   def create
-    @message = params[:message]
-    question_id = params[:question_id]
+    @message = feedback_params[:message]
+    question_id = feedback_params[:question_id]
 
     if @message.present?
       FeedbackMailer.send_feedback(
@@ -20,7 +21,13 @@ class FeedbacksController < ApplicationController
       flash.now[:notice] = "フィードバックを送信しました"
     else
       flash.now[:alert] = "メッセージを入力してください"
-      render status: :unprocessable_entity
+      render status: :unprocessable_content
     end
+  end
+
+  private
+
+  def feedback_params
+    params.permit(:message, :question_id)
   end
 end
