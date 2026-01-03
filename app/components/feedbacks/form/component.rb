a6e8e76138c09c64f61e1e38c749f8908cc82099
sekito <sekito1107gg@gmail.com>
2026-01-03 09:08:24 +0900

    柔軟にクラスを受け取れるように変更

--- app/components/feedbacks/form/component.rb
+++ app/components/feedbacks/form/component.rb
@@ -3,9 +3,10 @@
 module Feedbacks
   module Form
     class Component < ViewComponent::Base
-      def initialize(title: "フィードバック", question_id: nil)
+      def initialize(title:, question_id: nil, classes: nil)
         @title = title
         @question_id = question_id
+        @classes = classes
       end
     end
   end
