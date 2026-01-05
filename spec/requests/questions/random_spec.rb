cc954d4dc62850f67fc0488c4089e93d9d181593
sekito <sekito1107gg@gmail.com>
2026-01-05 09:45:27 +0900

    非ログイン状態で利用できる問題がなかった場合のテストケースを追加

--- spec/requests/questions/random_spec.rb
+++ spec/requests/questions/random_spec.rb
@@ -40,6 +40,19 @@ RSpec.describe "Questions::Random", type: :request do
           expect(response).to redirect_to(%r{/questions/\d+})
         end
       end
+      
+      context "利用可能な問題が1つもない場合" do
+        before do
+          Question.update_all(deleted_at: Time.current)
+        end
+
+        it "ルートパスへリダイレクトされ、アラートが表示される" do
+          get questions_random_path
+
+          expect(response).to redirect_to(root_path)
+          expect(flash[:alert]).to eq "現在利用可能な問題がありません"
+        end
+      end
     end
   end
 end
